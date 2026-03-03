e21bd008c313b8d5f61f6654ac92716996260a5e
Balaji R <manithilaibalaji@gmail.com>
2026-03-03 16:02:00 +0530

    Added login, dashboard, auth middleware, Tailwind setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,23 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+// const { Pool } = require('pg');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// module.exports = pool;
+
+// db.js
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: "postgres",
+  host: "localhost",
+  database: "nskillindia_db",
+  password: "1234",
+  port: 5432,
 });
 
 module.exports = pool;
